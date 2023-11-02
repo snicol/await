c4734160446ec47ec64fe5e1fdf19f38dfca6002
snicol <s.cott@me.com>
2023-11-02 15:43:50 +0000

    Add support for catching panics

---
+++ ctxerrgroup_test.go
@@ -0,0 +1,33 @@
+package await_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/snicol/await"
+)
+
+func TestCtxErrorGroupPanic(t *testing.T) {
+	ctx := context.Background()
+
+	g := await.Group(ctx)
+
+	g.Go(func(ctx context.Context) error {
+		panic("something went wrong")
+	})
+
+	err := g.Wait()
+	pe, ok := err.(await.PanicError)
+	if !ok {
+		t.Error("expected a panic error")
+		return
+	}
+
+	if pe.Error() != "goroutine panicked: something went wrong" {
+		t.Error("unexpected error found")
+	}
+
+	if pe.Panic != "something went wrong" {
+		t.Error("expected underlying panic to be set")
+	}
+}
